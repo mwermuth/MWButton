60ed36dffe770b4ee9e3206b7dea8dbd915fbf49
marcus <marcus@mwermuth.com>
2013-07-26 14:40:42 +0200

    Update MWButton.h

--- MWButton.h
+++ MWButton.h
@@ -1,9 +1,9 @@
 //
 //  MWButton.h
-//  nextr2
+//  mwermut
 //
 //  Created by Marcus on 18.07.13.
-//  Copyright (c) 2013 nextr.info. All rights reserved.
+//  Copyright (c) 2013 mwermuth.com. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
